3e783e6dda2bd09e13b729b7ed1d7a3993d87ef7
𝘚𝘪𝘺𝘢 ‹𝟹 <siyatechbot@gmail.com>
2026-02-10 02:57:58 +0530

    Update ping.py

--- Dev/plugins/ping.py
+++ Dev/plugins/ping.py
@@ -15,7 +15,7 @@ async def _ping(_, m: types.Message):
     uptime = get_time(int(time.time() - boot))
     latency = round((time.time() - start) * 1000, 2)
     await sent.edit_media(
-        media=types.InputMediaPhoto(
+        media=types.InputMediaVideo(
             media="https://ar-hosting.pages.dev/1770672280720.mp4",
             caption=m.lang["ping_pong"].format(
                 latency,
