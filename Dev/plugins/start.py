bb37950017e538056cf3d0ab543d1129561d86bd
𝘚𝘪𝘺𝘢 ‹𝟹 <siyatechbot@gmail.com>
2026-02-09 19:52:04 +0530

    Update start.py

--- Dev/plugins/start.py
+++ Dev/plugins/start.py
@@ -38,7 +38,7 @@ async def start(_, message: types.Message):
         reply_markup=key,
         quote=not private,
         parse_mode=enums.ParseMode.HTML,
-        effect_id=5242522100375237015
+        effect_id=5104841245755180586
     )
 
     if private:
