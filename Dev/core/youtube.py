f4ffc6ae3a100f2369004c084dbfd264b9c7f0fb
𝘚𝘪𝘺𝘢 ‹𝟹 <siyatechbot@gmail.com>
2026-02-10 17:15:02 +0530

    Update youtube.py

--- Dev/core/youtube.py
+++ Dev/core/youtube.py
@@ -5,7 +5,9 @@ import random
 import asyncio
 import aiohttp
 from pathlib import Path
+from typing import Optional, Union
 
+from pyrogram import enums, types
 from py_yt import Playlist, VideosSearch
 
 from Dev import logger
@@ -17,7 +19,6 @@ class YouTube:
         self.base = "https://www.youtube.com/watch?v="
         self.cookies = []
         self.checked = False
-        self.cookie_dir = "Dev/cookies"
         self.warned = False
         self.regex = re.compile(
             r"(https?://)?(www\.|m\.|music\.)?"
@@ -27,34 +28,59 @@ class YouTube:
 
     def get_cookies(self):
         if not self.checked:
-            for file in os.listdir(self.cookie_dir):
+            for file in os.listdir("Dev/cookies"):
                 if file.endswith(".txt"):
-                    self.cookies.append(f"{self.cookie_dir}/{file}")
+                    self.cookies.append(file)
             self.checked = True
         if not self.cookies:
             if not self.warned:
                 self.warned = True
                 logger.warning("Cookies are missing; downloads might fail.")
             return None
-        return random.choice(self.cookies)
+        return f"Dev/cookies/{random.choice(self.cookies)}"
 
     async def save_cookies(self, urls: list[str]) -> None:
         logger.info("Saving cookies from urls...")
         async with aiohttp.ClientSession() as session:
             for url in urls:
-                name = url.split("/")[-1]
-                link = "https://batbin.me/raw/" + name
+                path = f"Dev/cookies/cookie{random.randint(10000, 99999)}.txt"
+                link = url.replace("me/", "me/raw/")
                 async with session.get(link) as resp:
                     resp.raise_for_status()
-                    with open(f"{self.cookie_dir}/{name}.txt", "wb") as fw:
+                    with open(path, "wb") as fw:
                         fw.write(await resp.read())
-        logger.info(f"Cookies saved in {self.cookie_dir}.")
+        logger.info("Cookies saved.")
 
     def valid(self, url: str) -> bool:
         return bool(re.match(self.regex, url))
 
+    def url(self, message_1: types.Message) -> Union[str, None]:
+        messages = [message_1]
+        link = None
+        if message_1.reply_to_message:
+            messages.append(message_1.reply_to_message)
+
+        for message in messages:
+            text = message.text or message.caption or ""
+
+            if message.entities:
+                for entity in message.entities:
+                    if entity.type == enums.MessageEntityType.URL:
+                        link = text[entity.offset : entity.offset + entity.length]
+                        break
+
+            if message.caption_entities:
+                for entity in message.caption_entities:
+                    if entity.type == enums.MessageEntityType.TEXT_LINK:
+                        link = entity.url
+                        break
+
+        if link:
+            return link.split("&si")[0].split("?si")[0]
+        return None
+
     async def search(self, query: str, m_id: int, video: bool = False) -> Track | None:
-        _search = VideosSearch(query, limit=1, with_live=False)
+        _search = VideosSearch(query, limit=1)
         results = await _search.next()
         if results and results["result"]:
             data = results["result"][0]
@@ -94,7 +120,7 @@ class YouTube:
             pass
         return tracks
 
-    async def download(self, video_id: str, video: bool = False) -> str | None:
+    async def download(self, video_id: str, video: bool = False) -> Optional[str]:
         url = self.base + video_id
         ext = "mp4" if video else "webm"
         filename = f"downloads/{video_id}.{ext}"
@@ -131,10 +157,11 @@ class YouTube:
                 try:
                     ydl.download([url])
                 except (yt_dlp.utils.DownloadError, yt_dlp.utils.ExtractorError):
-                    if cookie: self.cookies.remove(cookie)
+                    if cookie in self.cookies:
+                        self.cookies.remove(cookie)
                     return None
                 except Exception as ex:
-                    logger.warning("Download failed: %s", ex)
+                    logger.error("Download failed: %s", ex)
                     return None
             return filename
 
