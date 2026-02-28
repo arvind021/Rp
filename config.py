13bcbbcf581a59708f9468d2ea9a0ccaa5da0e41
arvind021 <ak4392916@gmail.com>
2026-02-28 07:26:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
         self.API_HASH = "78730e89d196e160b0f1992018c6cb19"
 
         self.BOT_TOKEN = "8677717845:AAGt3sfCM30DhTa6Pjs1GllVaNUyzRaKFR8"
-        self.MONGO_URL = ""
+        self.MONGO_URL = "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority"
 
         self.LOGGER_ID = -1003763475049
         self.OWNER_ID = 6944519938
